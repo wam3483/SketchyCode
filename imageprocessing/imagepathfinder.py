54945a8cf81e9190e1b113b8f9cbef3200ddcc64
wam3483 <wam3483@gmail.com>
2025-04-17 17:35:15 -0400

    finished imagepathfinder. may rename later. finds contiguous selections of black pixels in an image.

--- imageprocessing/imagepathfinder.py
+++ imageprocessing/imagepathfinder.py
@@ -1,57 +1,48 @@
-import threading
 from PIL import Image
-import time
-from typing import Tuple, Optional
+from typing import Tuple, List
 from collections import deque
 
+Point = Tuple[int, int]
+Region = List[Point]
+
 class ImagePathFinder:
-    def find_path(self, bitmapImage : Image.ImageFile) -> Tuple[int, int]:
-        """
-            Scans the image and returns the coordinates of the starting point of a path.
-            Returns:
-                Tuple[int, int]: Coordinates of the start of the path, or (-1, -1) if not found.
-        """
+
+    def __init__(self, bitmap_img : Image.Image, min_region_size = 10):
+        self._img = bitmap_img
+        self._min_region_size = min_region_size
+
+    def find_regions(self) -> List[Region]:
+        regions = []
         visited_points = set()
-        for y in range(0, bitmapImage.height):
-            for x in range(0, bitmapImage.width):
-                if not (x,y) in visited_points:
-                    visited_points.add((x,y))
-                    self._find_region((x,y), visited_points, bitmapImage)
+        for y in range(0, self._img.height):
+            for x in range(0, self._img.width):
+                if not (x,y) in visited_points and self._is_pixel_black((x,y)):
+                        region = self._select_pixels_from((x,y), visited_points)
+                        if len(region) > self._min_region_size:
+                            regions.append(region)
+        return regions
 
-        return (-1, -1)
+    def _is_pixel_black(self, point: Point) -> bool:
+        pixel = self._img.getpixel(point)
+        return pixel == (0, 0, 0) or (isinstance(pixel, int) and pixel == 0)
 
-    def _find_closest_pixel_to_origin(self, image: Image.ImageFile) -> Optional[Tuple[int,int]]:
-        min_distance = image.width * image.height
-        start_x = -1
-        start_y = -1
-        for y in range(0, image.height):
-            if y*y > min_distance:
-                break
-            for x in range(0, image.width):
-                pixel = image.getpixel(x, y)
-                if pixel == (0, 0, 0) or (isinstance(pixel, int) and pixel == 0):
-                    distance = x*x + y*y
-                    if distance < min_distance:
-                        min_distance = distance
-                        start_x = x
-                        start_y = y
+    def _select_pixels_from(self, point : Point, visited_points : set[Point]) -> Region:
+        stack = deque()
+        stack.append(point)
+        selected_region = Region()
+        while stack:
+            next_point = stack.pop()
+            x,y = next_point
 
-        if start_x == -1 and start_y == -1:
-            return None
-
-        return (start_x, start_y)
-
-
-    def _find_region(self,  start_point : Tuple[int,int], visited_points : set, image: Image.ImageFile) -> Optional[Tuple[int,int]]:
-        x_start, y_start = start_point
-
-        for y in range(y_start, image.height):
-            for x in range(x_start, image.width):
-                if not (x,y) in visited_points:
-                    visited_points.add((x,y))
-                    pixel = image.getpixel(x, y)
-                    if pixel == (0, 0, 0) or (isinstance(pixel, int) and pixel == 0):
-                        return (x, y)
-
-        return None
-
+            if (0 <= x < self._img.width and 0 <= y < self._img.height
+                and not next_point in visited_points
+                and self._is_pixel_black(next_point)):
+                    visited_points.add(next_point)
+                    selected_region.append(next_point)
+                    stack.extend([
+                        (x, y+1),
+                        (x-1, y),
+                        (x+1, y),
+                        (x, y-1)
+                    ])
+        return selected_region
